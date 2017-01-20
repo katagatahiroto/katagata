ccdee57479993b15c8f1ccc1638db4d9ef1c862e
wordi <wordijp@gmail.com>
2017-01-21 03:10:13 +0900

    開発用に切り替えるコードを追加
    開発中でもメール認証をするのが理想だが、無効化の方が手っ取りばやい

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
-
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable
+    #:recoverable, :rememberable, :trackable, :validatable # メール認証をしない場合はこちら
+  
   validates :name, presence: true, on: :update
   validates :nickname, presence: true, on: :update, length: { in: 2..8 }
   validates :number, presence: true, on: :update, length: { is: 7 } ,numericality: { only_integer: true }
