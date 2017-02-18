78ea6ccd98fb01b01713e643d569343529b02ea3
katagatahiroto <korosuke1105@gmail.com>
2017-02-19 07:47:57 +0900

    変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,7 +37,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_fit => [200, 150]
+     process :resize_to_fit => [400, 300]
   end
 
 
