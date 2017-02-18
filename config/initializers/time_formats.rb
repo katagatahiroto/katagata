68186cd5093b1c2dca5485f9acb8c313d4a54f5d
katagatahiroto <korosuke1105@gmail.com>
2017-02-19 07:56:48 +0900

    Add files via upload

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,6 @@
+# coding: utf-8
 Time::DATE_FORMATS[:published_on] = "%Y年%m月%d日 %H時%M分"
-Date::DATE_FORMATS[:default]      = "%Y年%m月d日"
+Time::DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
+Time::DATE_FORMATS[:HM] = " %H時%M分"
+Time::DATE_FORMATS[:mdHM] = "%m月%d日%H時%M分"
+Date::DATE_FORMATS[:default]      = "%Y年%m月%d日"
