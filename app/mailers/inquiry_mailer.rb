900a20ff432fcc1939805673032f9e56d82bfa8a
katagatahiroto <korosuke1105@gmail.com>
2017-02-20 12:37:51 +0900

    Update inquiry_mailer.rb

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,7 +1,7 @@
 class InquiryMailer < ActionMailer::Base
-  default from: "information@ticket-navi.net"    # 送信元アドレス
-  default to: "support-help@ticket-navi.net"     # 送信先アドレス
-  default to: "support@ticket-navi.net"          # 送信先アドレス
+  default from: "otoku-na@highlows.jp"    # 送信元アドレス
+  default to: "otoku-na@highlows.jp"     # 送信先アドレス
+  default to: "support@highlows.jp"     # 送信先アドレス
   def received_email(inquiry)
     @inquiry = inquiry
     mail(:subject => 'お問い合わせを承りました')
