c2f902fda97fd412bd3de300053a2022b7f30e14
katagatahiroto <korosuke1105@gmail.com>
2017-02-20 12:44:21 +0900

    Update inquiry_mailer.rb

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,6 @@
 class InquiryMailer < ActionMailer::Base
   default from: "otoku-na@highlows.jp"    # 送信元アドレス
-  default to: "otoku-na@highlows.jp"     # 送信先アドレス
+  default to: "support-help@highlows.jp"     # 送信先アドレス
   default to: "support@highlows.jp"     # 送信先アドレス
   def received_email(inquiry)
     @inquiry = inquiry
