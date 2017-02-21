c57a225901147383a8397e055e0a813f80049192
katagatahiroto <korosuke1105@gmail.com>
2017-02-21 12:55:14 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,5 @@
 CarrierWave.configure do |config|
- require 'carrierwave/storage/abstract'
- require 'carrierwave/storage/file'
- require 'carrierwave/storage/fog'
-    config.storage = :fog
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AKIAJPQP6XZKDINATPEA'],
