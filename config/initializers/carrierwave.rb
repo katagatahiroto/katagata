af5be816d991a5d45caf6e392aad85517c6caa44
katagatahiroto <korosuke1105@gmail.com>
2017-02-21 13:06:20 +0900

    Add files via upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+    # config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
@@ -6,12 +7,12 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['nQ1W3tOQfXCtQ1zx5/sF1mkX7I4GlrrQvljF2Xm6'],
     region: 'us-east-1'
   }
+config.fog_public     = true # public-read
+
+config.remove_previously_stored_files_after_update = false
 
 
     case Rails.env
-    when 'development'
-        config.fog_directory  = 'otoku-na'
-        config.asset_host = 'https://s3.amazonaws.com/otoku-na'
     when 'production'
         config.fog_directory  = 'otoku-na'
         config.asset_host = 'https://s3.amazonaws.com/otoku-na'
