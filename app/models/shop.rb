a55b0dcc46fa2d57fe82e8fe4e3c7a0a655a36f0
katagatahiroto <korosuke1105@gmail.com>
2017-02-26 09:24:06 +0900

    Add files via upload

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -12,60 +12,47 @@ class Shop < ActiveRecord::Base
 
   attr_accessor :close_date, :close_time
 
-  enum list_price: {men: 10 ,women: 20 ,others: 30}
+  enum list_price: {men: 10 ,women: 20 ,full: 30 ,others: 40}
   enum shipping_method: {post: 30 ,hand_sent: 60}
   enum ticketing_state: {have: 700 ,not_have: 800}
   enum postage: {postage_in: 91 , postage_out: 92}
   enum nsk: {seiki: 93 ,c2c: 94 ,present: 95 ,other: 96}
   enum ticket_name_yes_no: {name_yes: 101 ,name_no: 102}
   enum docide_promptly: {within_24_hours: 200 ,within_3day: 300 ,within_7day: 400 ,other_day:500}
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  enum accessories: {hi: 111 ,low: 222}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1401 }
+  validates :ref_price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def price=(value)
     value.tr!('０-９', '0-9') if value.is_a?(String)
     super(value)
   end
-  validates :airt_name, presence: true
-  validates :live_tour_name, presence: true
-  validates :day_date, presence: true
-  validates :close_datetime, presence: true
-  validates :plase, presence: true
-  #validates :list_price, presence: true
-  validates :number_of_sheets, presence: true
-  validates :seat, presence: true
-  validates :shipping_method, presence: true
-  #validates :ticketing_state, presence: true
-  validates :postage, presence: true
-  #validates :nsk, presence: true
-  #validates :ticket_name, presence: true
-  #validates :ticket_name_yes_no, presence: true
-  validates :docide_promptly, presence: true
-  #validates :othertext, presence: true
 
-# 変更後のDB関係
-  #validates :airt_name, presence: true
-  #validates :female, presence: true
-  #validates :big_tags, presence: true
-  #validates :sub_tags, presence: true
-  #validates :state, presence: true
-  #validates :day_date, presence: true
-  #validates :price, presence: true
-  #validates :ref_price, presence: true
-  #validates :certificate, presence: true
-  #validates :color, presence: true
-  #validates :size, presence: true
-  #validates :material, presence: true
-  #validates :number, presence: true
-  #validates :nsk, presence: true
-  #validates :ticket_name_yes_no, presence: true
-  #validates :shipping_method, presence: true
-  #validates :postage, presence: true
-  #validates :docide_promptly, presence: true
-  #validates :close_date, presence: true
-  #validates :image, presence: true
-  #validates :comment_text, presence: true
-  #validates :state_info, presence: true
-# ここまでが変更後反映
+  validates :airt_name, presence: true
+  validates :female, presence: true
+  validates :big_tags, presence: true
+  validates :sub_tags, presence: true
+  validates :state, presence: true
+  validates :day_date, presence: true
+  validates :price, presence: true
+  validates :ref_price, presence: true
+  validates :certificate, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
+  validates :material, presence: true
+  validates :number, presence: true
+  validates :nsk, presence: true
+  validates :ticket_name_yes_no, presence: true
+  validates :shipping_method, presence: true
+  validates :postage, presence: true
+  validates :docide_promptly, presence: true
+  validates :close_date, presence: true
+  validates :comment_text, presence: true
+  validates :state_info, presence: true
+  validates :madein, presence: true
+  validates :accessories, presence: true
+  validates :item_name, presence: true
+
 
   # before_save :set_close_datetime
 
