c58059e02b952b0c5cc2c1930af13722f65c1a3b
katagatahiroto <korosuke1105@gmail.com>
2017-03-05 01:54:42 +0900

    Add files via upload

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,7 +1,7 @@
 class InquiryMailer < ActionMailer::Base
   default from: "otoku-na@highlows.jp"    # 送信元アドレス
   default to: "support-help@highlows.jp"     # 送信先アドレス
-  default to: "support@highlows.jp"     # 送信先アドレス
+  default to: "support@highlows.jp"          # 送信先アドレス
   def received_email(inquiry)
     @inquiry = inquiry
     mail(:subject => 'お問い合わせを承りました')
